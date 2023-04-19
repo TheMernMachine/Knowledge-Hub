77fd33c8302febb45f3e634e2e02aec4b3a37454
zcord <zeus.cordeiro@gmail.com>
2023-04-18 23:37:06 -0400

    profile pic

--- server/models/User.js
+++ server/models/User.js
@@ -31,6 +31,10 @@ const UserSchema = new Schema({
     required: true,
     minlength: 5,
   },
+  profilePic: {
+    type: String,
+    default: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png',
+  },
   role: {
     type: Types.Schema.ObjectId,
     ref: 'Role',
