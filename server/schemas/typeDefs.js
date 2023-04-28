ef74d1d87f36be1d21a0ab81fd41c9a44f041254
Chris Girard <chrisegirard@gmail.com>
2023-04-27 23:24:54 -0400

    Added actual resolver and typedef for adding student to course

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -184,6 +184,7 @@ const typeDefs = gql`
         
         addCourse(title: String!, description: String!, startDate: String!, endDate: String!, price: Float!, teacher: ID!): Course
         updateCourse(_id: ID!, title: String, description: String, price: Float, quiz: [ID], assignment: [ID], lessonNotes: [ID], startDate: String, endDate: String): Course
+        addStudentToCourse(courseId: ID!, studentId: ID!): Course
         deleteCourse(_id: ID!): Course
 
         addRole(name: String!, permissions: [String]!): Role
