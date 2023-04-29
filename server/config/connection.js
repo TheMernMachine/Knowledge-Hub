98b2345b9c9c64da9935ea3ac67430177cd43055
zcord <zeus.cordeiro@gmail.com>
2023-04-28 22:53:58 -0400

    database fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/KnowledgeHub-DB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/KnowledgeHub-DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
