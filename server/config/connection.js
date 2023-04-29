3a6fffa13f60d563f65023975596914defc97617
zcord <zeus.cordeiro@gmail.com>
2023-04-28 22:42:24 -0400

    database fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/KnowledgeHub-DB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/KnowledgeHub-DB');
 
 module.exports = mongoose.connection;
