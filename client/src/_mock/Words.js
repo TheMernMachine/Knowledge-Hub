704508d191439516bed8f01693f96c50a3ad4a45
zcord <zeus.cordeiro@gmail.com>
2023-04-28 21:01:57 -0400

    Dashboard polish

---
+++ client/src/_mock/Words.js
@@ -0,0 +1,13 @@
+import { faker } from "@faker-js/faker";
+
+// ----------------------------------------------------------------------
+
+// const word = faker.random.word();
+
+
+
+
+
+const words = ["bowdlerize", "pedagogue", "obsequious", "quotidian", "evanescent", "abjure", "centrifugal", "diffident", "enervate", "fiduciary", "homogeneous", "incognito", "loquacious", "nihilism", "parabola", "recapitulate", "sanguine", "tectonic", "unctuous", "vacuous", "wrought", "yeoman", "ziggurat", "eulogy", "pillory", "charisma", "annulment", "smattering", "jeopardize", "risibility" ];
+
+export default words;
