3c87fa883cd9ca5e39793016d53bd65ba57ea8b3
zcord <zeus.cordeiro@gmail.com>
2023-04-28 23:29:54 -0400

    database adjustments for deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+require('dotenv').config()
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
